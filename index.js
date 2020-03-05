22dca2de4ccea9d670a88d9f249cffa9a034d730
eqflash <57890642+eqflash@users.noreply.github.com>
2020-03-05 15:44:24 +0800

    Add files via upload
    Challenge
    Muhammad Junaedi
    1701020015

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let todayWindow;
 let createWindow;
 let listWindow;
 
+
 app.on("ready", () => {
     todayWindow = new BrowserWindow({
         webPreferences: {
@@ -25,4 +26,101 @@ app.on("ready", () => {
         app.quit();
         todayWindow = null;
     });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
+};
+
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`file://${__dirname}/about.html`);
+    aboutWindow.on("closed", () => (createWindow = null));
+};
+
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    console.log(appointment);
+});
+
+
+const menuTemplate = [{
+            label: "File",
+            submenu: [{
+                    label: "New Appointment",
+
+                    click() {
+                        createWindowCreator();
+                    }
+            },
+            {
+                    label: "All Appointment",
+                    click() {
+                        listWindowCreator();
+                    }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl + Q",
+                click() {
+                    app.quit();
+                }  
+            }
+        ]
+    },
+
+    {
+
+        label: "View",
+        submenu: [{ role: "reload" }, { role: "toggledevtools" }]
+    },
+
+    {
+        label: "About",
+        submenu: [{
+            label: "My Profile",
+
+            click(){
+                aboutWindowCreator()
+            }
+        }]
+    }
+]
